1a4a682c528aca23d8d87d1299bce6c510b96145
chimichi004 <chember004@gmail.com>
2021-05-29 15:39:45 +0800

    first commit

---
+++ config/db.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+
+//mongoose.connect(db);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db,{
+      useNewUrlParser: true,
+      //Current Server Discovery and Monitoring engine is deprecated, 
+      //and will be removed in a future version. 
+      //To use the new Server Discover and Monitoring engine,
+      //pass option { useUnifiedTopology: true } to the MongoClient
+      //constructor. 
+      useUnifiedTopology: true
+    });
+
+    console.log('MongoDB Connected...');
+  }catch(err){
+    console.error(err.message);
+    //process exist with failure 
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
